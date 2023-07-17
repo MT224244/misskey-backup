53c2c7d43f8daa3f653fe1668ee8448fabd7719b
MT224244 <25514849+MT224244@users.noreply.github.com>
2023-07-17 22:34:17 +0900

    🚚 バックアップファイルの名前を変更 (yyyyMMddHHmm -> yyyyMMddHH)

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BACKUP_FILE="/backups/${POSTGRES_DB}_$(TZ='Asia/Tokyo' date +%Y%m%d%H%M).sql"
+BACKUP_FILE="/backups/${POSTGRES_DB}_$(TZ='Asia/Tokyo' date +%Y%m%d%H).sql"
 COMPRESSED="${BACKUP_FILE}.7z"
 
 pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB > $BACKUP_FILE 2>> /var/log/cron.log
